6213050bc20ac45018c5c690bbe22cfd7d07b136
vinodmadhu97 <vinodmadhu97@gmail.com>
2021-08-24 15:31:59 +0530

    speech to text applied

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class MainScreen extends StatefulWidget {
 
@@ -9,9 +13,36 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  String _speechText = "TAP START TO SPEECH";
+
   bool isListening = false;
-  String text = "Hello Emely \n How can i help You?";
+  bool _hasSpeech = false;
+  bool _logEvents = false;
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String lastWords = 'Hello Emely \n How can i help You?';
+  String lastError = '';
+  String lastStatus = '';
+  String _currentLocaleId = 'en_US';
+  final SpeechToText speech = SpeechToText();
+
+  //INITIALIZE THE SPEECH INSTANCE
+  Future<void> initSpeechState() async {
+    _logEvent('Initialize');
+    var hasSpeech = await speech.initialize(
+        onError: errorListener,
+        onStatus: statusListener,
+        debugLogging: true,
+        finalTimeout: Duration(milliseconds: 0));
+
+
+    if (!mounted) return;
+
+    setState(() {
+      _hasSpeech = hasSpeech;
+    });
+
+  }
 
 
 
@@ -19,10 +50,13 @@ class _MainScreenState extends State<MainScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    //INITIALIZE THE SPEECH INSTANCE WHEN APPLICATION STARTED
+    initSpeechState();
 
   }
   @override
   Widget build(BuildContext context) {
+    //MAIN WINDOW USER INTERFACES
     return Container(
       decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -59,7 +93,7 @@ class _MainScreenState extends State<MainScreen> {
 
                       ],
                     ),
-                    Text( !isListening ? _speechText : "Speeching!", style: TextStyle(
+                    Text( !isListening ? "TAP START TO SPEECH" : "LISTENING!", style: TextStyle(
                       color: Colors.white,
                       fontSize: 20,
                       fontWeight: FontWeight.bold
@@ -71,8 +105,8 @@ class _MainScreenState extends State<MainScreen> {
                       animate: isListening,
                       glowColor: Color(0xFFF7CAD0),
                       endRadius: 150.0,
-                      duration: Duration(milliseconds: 1000),
-                      repeatPauseDuration: Duration(milliseconds: 800),
+                      duration: Duration(milliseconds: 800),
+                      repeatPauseDuration: Duration(milliseconds: 600),
                       repeat: true,
                       child: Container(
                         width: 150.0,
@@ -86,34 +120,37 @@ class _MainScreenState extends State<MainScreen> {
                           elevation: 0.0,
                           child: Icon(isListening ? Icons.mic : Icons.mic_none,color: Color(0xFFFF5C8A),size: 60,),
                           onPressed: () {
-                           // onListen();
-                            //toggleRecording();
+
+                            //STARTING BUTTON GLOWER AND SOUND LISTENER
                             if(!isListening){
                               setState(() {
                                 isListening = true;
+                                startListening();
                               });
 
                             }else{
+                              //STOPPING BUTTON GLOWER AND LISTENER WHEN PRESSING AGAIN
                               setState(() {
                                 isListening = false;
+                                speech.stop();
                               });
                             }
                           },
                         ),
                       ),
-
-
-
-
                     ),
 
                     SizedBox(height: 100,),
-                    Text(text,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      //HERE SHOW THE RECOGNIZED TEXT FROM LISTENER
+                      child: Text(lastWords,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
 
@@ -127,8 +164,69 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
+  void statusListener(String status) {
+    _logEvent(
+        'Received listener status: $status, listening: ${speech.isListening}');
+    setState(() {
+      lastStatus = '$status';
+    });
+  }
 
+  void errorListener(SpeechRecognitionError error) {
+    _logEvent(
+        'Received error status: $error, listening: ${speech.isListening}');
+    setState(() {
+      lastError = '${error.errorMsg} - ${error.permanent}';
+    });
+  }
 
+  void _logEvent(String eventDescription) {
+    if (_logEvents) {
+      var eventTime = DateTime.now().toIso8601String();
+      print('$eventTime $eventDescription');
+    }
+  }
+
+    //SPEECH LISTENER
+  void startListening() {
+    setState(() {
+      isListening = true;
+    });
+
+    _logEvent('start listening');
+    lastWords = '';
+    lastError = '';
+    speech.listen(
+        onResult: resultListener,
+        listenFor: Duration(seconds: 30),
+        pauseFor: Duration(seconds: 5),
+        partialResults: true,
+        localeId: _currentLocaleId,
+        onSoundLevelChange: soundLevelListener,
+        cancelOnError: true,
+        listenMode: ListenMode.confirmation);
+    setState(() {});
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    _logEvent(
+        'Result listener final: ${result.finalResult}, words: ${result.recognizedWords}');
+    setState(() {
+
+      lastWords = '${result.recognizedWords}';
+      isListening = !result.finalResult;//button glowing is ended when the stop speeching.
+
+    });
+  }
+
+  void soundLevelListener(double level) {
+    minSoundLevel = min(minSoundLevel, level);
+    maxSoundLevel = max(maxSoundLevel, level);
+    _logEvent('sound level $level: $minSoundLevel - $maxSoundLevel ');
+    setState(() {
+      this.level = level;
+    });
+  }
 
 }
 
